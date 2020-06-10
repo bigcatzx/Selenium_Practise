a480de6b5b8790e4fe5b4899a915b5085326464a
bigcatzx <hzx4713972@126.com>
2020-06-10 16:34:32 +0800

    add add_cookie() and for in cookies

--- WebDriverAPI/Cookie.py
+++ WebDriverAPI/Cookie.py
@@ -2,8 +2,13 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("http://www.baidu.com")
 
-#获取所有Cookie信息并打印
+# 获取所有Cookie信息并打印
 cookie = driver.get_cookies()
 print(cookie)
 
-#添加Cookie信息
+# 添加Cookie信息
+driver.add_cookie({'name':'key-aaaaaaaa', 'value':'value-bbbbbbb'})
+
+# 遍历指定的Cookies
+for cookie in driver.get_cookies():
+    print("%s -> %s" % (cookie['name'], cookie['value']))
