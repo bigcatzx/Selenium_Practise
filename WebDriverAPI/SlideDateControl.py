0622add15aa86e0f9fd8645a728dfa913f7366fc
bigcatzx <hzx4713972@126.com>
2020-06-16 17:22:34 +0800

    Slide Date Toolbar and save screenshot Demo

--- WebDriverAPI/SlideDateControl.py
+++ WebDriverAPI/SlideDateControl.py
@@ -1,1 +1,25 @@
-from time
+from time import sleep
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("http://www.jq22.com/yanshi4976")
+sleep(2)
+
+driver.switch_to.frame("iframe")
+driver.find_element_by_id("appDate").click()
+sleep(2)
+
+# 定位要滑动的年月日
+dwwos = driver.find_elements_by_class_name("dwwo")
+year = dwwos[0]
+month = dwwos[1]
+day = dwwos[2]
+
+action = webdriver.TouchActions(driver)
+action.scroll_from_element(year, 0, 5).perform()
+action.scroll_from_element(month, 0, 30).perform()
+action.scroll_from_element(day, 0, 30).perform()
+
+sleep(3)
+driver.save_screenshot("./files/test_img.png")
+driver.quit()
