b348b1329711ea5bc8b5e23c2fa666d59874746d
bigcatzx <hzx4713972@126.com>
2020-06-17 18:13:59 +0800

    Auto test module

---
+++ AutoTestModule/class/module.py
@@ -0,0 +1,2 @@
+# class Mail:
+#  def __init__(self):
---
+++ AutoTestModule/script/test_mail.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("http://www.126.com")
+
+# 登录
+sleep(2)
+email_frame = driver.find_element_by_xpath("//div[@id='loginDiv']/iframe")
+driver.switch_to.frame(email_frame)
+driver.find_element_by_name('email').send_keys("hzx4713972")
+driver.find_element_by_name('password').send_keys("HZX-+*zry_")
+driver.find_element_by_id('dologin').click()
+
+sleep(5)
+
+#退出
+driver.find_element_by_link_text("退出").click()
+
+driver.quit()
